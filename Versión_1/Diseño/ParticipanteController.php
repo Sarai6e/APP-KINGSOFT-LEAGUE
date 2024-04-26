31269ea67f8ee640389508a48f0915621d325272
Sarai6e <1472173@senati.pe>
2024-04-26 09:47:23 -0500

    correccion de participantes

--- Versión_1/Diseño/ParticipanteController.php
+++ Versión_1/Diseño/ParticipanteController.php
@@ -1,12 +1,16 @@
 <?php
-class ParticipanteModel {
-    private $conn;
+require_once 'ParticipanteModel.php';
+
+class ParticipanteController {
+    private $model;
 
     public function __construct($db) {
+        $this->model = new ParticipanteModel($db);
         $this->conn = $db;
     }
 
     public function getAllParticipantes() {
+        return $this->model->getAllParticipantes();
         $query = "SELECT * FROM participantes";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -14,6 +18,7 @@ class ParticipanteModel {
     }
 
     public function getParticipanteById($id) {
+        return $this->model->getParticipanteById($id);
         $query = "SELECT * FROM participantes WHERE id = ?";
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$id]);
@@ -21,15 +26,18 @@ class ParticipanteModel {
     }
 
     public function updateParticipante($id, $nombre, $apellido, $dni, $genero_id, $grado_estudio_id, $año_estudio, $especialidad, $correo, $clave, $robot_id, $club_robotica_id, $fecha_nacimiento) {
+        return $this->model->updateParticipante($id, $nombre, $apellido, $dni, $genero_id, $grado_estudio_id, $año_estudio, $especialidad, $correo, $clave, $robot_id, $club_robotica_id, $fecha_nacimiento);
         $query = "UPDATE participantes SET nombre = ?, apellido = ?, dni = ?, participante_genero_id = ?, grado_estudio_id = ?, año_estudio = ?, especialidad = ?, correo = ?, clave = ?, robot_id = ?, club_robotica_id = ?, fecha_nacimiento = ? WHERE id = ?";
         $stmt = $this->conn->prepare($query);
         return $stmt->execute([$nombre, $apellido, $dni, $genero_id, $grado_estudio_id, $año_estudio, $especialidad, $correo, $clave, $robot_id, $club_robotica_id, $fecha_nacimiento, $id]);
     }
 
     public function deleteParticipante($id) {
+        return $this->model->deleteParticipante($id);
         $query = "DELETE FROM participantes WHERE id = ?";
         $stmt = $this->conn->prepare($query);
         return $stmt->execute([$id]);
     }
 }
+
 ?>
--- Versión_1/Diseño/ParticipanteModel.php
+++ Versión_1/Diseño/ParticipanteModel.php
@@ -6,9 +6,24 @@ class ParticipanteModel {
         $this->conn = $db;
     }
 
+    public function getAllParticipantes() {
+        $query = "SELECT * FROM participantes WHERE id = 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+    public function getParticipanteById($id) {
+        $query = "SELECT * FROM participantes WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+
     public function updateParticipante($id, $nombre, $apellido, $dni, $genero_id, $grado_estudio_id, $año_estudio, $especialidad, $correo, $clave, $robot_id, $club_robotica_id, $fecha_nacimiento) {
         $query = "UPDATE participantes SET nombre = ?, apellido = ?, dni = ?, participante_genero_id = ?, grado_estudio_id = ?, año_estudio = ?, especialidad = ?, correo = ?, clave = ?, robot_id = ?, club_robotica_id = ?, fecha_nacimiento = ? WHERE id = ?";
-        
+
         // Aquí se comprueba si el ID es 1
         if ($id == 1) {
             $query = "UPDATE participantes SET nombre = ?, apellido = ?, dni = ?, participante_genero_id = ?, grado_estudio_id = ?, año_estudio = ?, especialidad = ?, correo = ?, clave = ?, robot_id = ?, club_robotica_id = ?, fecha_nacimiento = ? WHERE id = 1";
@@ -20,7 +35,7 @@ class ParticipanteModel {
 
     public function deleteParticipante($id) {
         $query = "DELETE FROM participantes WHERE id = ?";
-        
+
         // Aquí se comprueba si el ID es 1
         if ($id == 1) {
             $query = "DELETE FROM participantes WHERE id = 1";
