8500db81cc539c3eaad3a41c9e2eda21a15f1274
Sarai6e <1472173@senati.pe>
2024-04-27 09:59:56 -0500

    27-04-24

---
+++ Versión_1/Diseño/agregar_competencia.php
@@ -0,0 +1,29 @@
+<?php 
+include("./app/config.php");
+include("./layout/sesion.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Competencias</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .table{
+            background-color:white;
+        }
+        .container {
+            margin-top: 150px; /* Ajuste del margen superior */
+            text-align: center; /* Centrar elementos dentro del contenedor */
+        }
+        h1{
+             color:white;
+        }
+    </style>
+</head>
+<body>
+<?php include 'navegador.php'; ?>
+<div class="container">
